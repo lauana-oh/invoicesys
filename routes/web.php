4fc1292f5466212d35ea6da1d8f0c9cdd083cd43
lau-hubler <lauanahubler@gmail.com>
2019-11-18 10:59:49 -0500

    Created route for products resource, product.index in ProductController and blade

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*------Authentication's Routes----*/
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*------Products' Routes-----------*/
+Route::resource('/products','ProductController');
