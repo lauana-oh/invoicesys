54ed9af2df4e29b5d4e9a84abbe73ff160863b80
lau-hubler <lauanahubler@gmail.com>
2019-11-19 09:10:09 -0500

    Created Invoice Model -mr and updated migration data

---
+++ database/migrations/2019_11_19_140213_create_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('client_id');
+            $table->unsignedInteger('vendor_id');
+            $table->date('invoice_date');
+            $table->date('delivery_date');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
