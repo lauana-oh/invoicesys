86e10bec83b64964202dad560abd9bfb09e828d9
lau-hubler <lauanahubler@gmail.com>
2019-11-28 13:52:29 -0500

    Created Statuses seeder and migration, add status_id in invoice factory and migration, update env.example with invoicesys name

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -12,5 +12,6 @@ $factory->define(Invoice::class, function (Faker $faker) {
         'invoice_date' => $faker->date('Y-m-d', 'delivery_date'),
         'delivery_date' => $faker->date('Y-m-d', 'due_date'),
         'due_date' => $faker->date('Y-m-d', '+1 month'),
+        'status_id' => \App\Status::all()->keyBy('id')->keys()->random(),
     ];
 });
